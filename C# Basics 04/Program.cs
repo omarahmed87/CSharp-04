8b6a5bbbc8c4dc647faa8caad32e88513079dd4c
Omar Ahmed <cresssoomar@gmail.com>
2026-09-07 20:49:21 +0300

    1st answer

--- C# Basics 04/Program.cs
+++ C# Basics 04/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿#region 1st answer
+/*
+1-Declare string title = "clean code";. Call title.ToUpper()and store it in a new variable
+upperTitle. Print both title and upperTitle to show that title did not change.
+  */
+#endregion
+
+class program 
+{
+    static void Main(string[] args)
+    {
+        string title = "clean code";
+        string upperTitle = title.ToUpper();
+        Console.WriteLine($"Original title: {title}");
+        Console.WriteLine($"Uppercase title: {upperTitle}");
+    }
+}
